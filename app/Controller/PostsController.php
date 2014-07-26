3561033fd6018a9ee0a4dcbd7a78ce9c29d017cf
Vipul Nandan <vipnandan@gmail.com>
2014-07-27 04:10:53 +0530

    Testing of algo recmmend

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -14,14 +14,48 @@ class PostsController extends AppController {
             $this->request->data['Post']['user_id'] = $this->Session->read('Auth.User.id');
             if ($this->Post->save($this->request->data))
             {
-            	$set1 = explode(',', $this->request->data['Post']['statement']);
-            	$set2 = explode(',', $this->request->data['Post']['description']);
+            	$set1 = explode(' ', $this->request->data['Post']['statement']);
+            	$set2 = explode(' ', $this->request->data['Post']['description']);
 
-            	$refWords = array('MS excel');
-            	
+            	foreach ($set1 as $Word) {
+            		$Word = trim($Word);
+            		$Word = stripslashes($Word);
+            		$Word = strtolower($Word);
+            	}
+
+            	foreach ($set2 as $Word) {
+            		$Word = trim($Word);
+            		$Word = stripslashes($Word);
+            		$Word = strtolower($Word);
+            	}
+
+            	$referenceWords = array();
+            	$refWords = $this->Post->query('select * from refkeywords');
+            	//debug($refWords);
+            	foreach ($refWords as $Word)
+            	{
+            	$referenceWords[] = strtolower($Word['refkeywords']['word']);
+            	}
+
             	$mergeWords = array_merge($set1,$set2);
 
-            	$set4 = array_intersect($mergeWords, $refWords)
+            	$set4 = array_intersect($mergeWords, $referenceWords);
+            	$set4 = array_unique($set4);
+
+            	$query = "select count(*),post_id,p.* from keywords as k inner join posts as p on k.post_id =p.id  where k.word in (select name from skills where user_id = ".$this->Session->read('Auth.User.id').") group by k.post_id order by count(*) Desc LIMIT 10";
+            	$result = $this->Post->query($query);
+
+            	$this->set('recPosts',$result);
+
+            	$data = array();
+
+            	foreach ($set4 as $Word) {
+            		$Word = trim($Word);
+            		$Word = stripslashes($Word);
+            		$data[] = array('word'=>$Word,'post_id'=>$this->Post->getLastInsertId());
+            	}
+            	
+            	$this->Post->Keyword->saveMany($data);
 
             	$this->Session->setFlash(__('Question created!'));
             	return $this->redirect('/users/dashboard');
